6caa15392ad981fbb73d8c4bc5ad3be0ba280c1e
xrelkd <46590321+xrelkd@users.noreply.github.com>
2022-04-04 18:02:01 +0800

    feat: use default value to deserialize field in `Config` when the value is not present

--- src/config.rs
+++ src/config.rs
@@ -16,10 +16,19 @@ pub struct SymbolLink {
 #[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Config {
+    #[serde(default)]
     pub commands: Vec<Vec<String>>,
+
+    #[serde(default)]
     pub directories: Vec<PathBuf>,
+
+    #[serde(default)]
     pub empty_files: Vec<PathBuf>,
+
+    #[serde(default)]
     pub links: HashMap<PathBuf, PathBuf>,
+
+    #[serde(default)]
     pub copies: HashMap<PathBuf, PathBuf>,
 }
 
@@ -39,5 +48,69 @@ impl Config {
             .context(error::ReadConfigFileSnafu { file_path: config_file.to_owned() })?;
 
         Self::from_yaml(&data)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{collections::HashMap, path::PathBuf};
+
+    use super::Config;
+
+    #[test]
+    fn test_parse_yaml() {
+        assert!(Config::from_yaml("").is_err());
+
+        let config_text = r#"{}"#;
+        let cfg = Config::from_yaml(config_text).unwrap();
+        assert_eq!(cfg, Config::default());
+
+        let config_text = r#"
+---
+commands: []
+directories: []
+emptyFiles: []
+links: {}
+copies: {}
+"#;
+
+        let cfg = Config::from_yaml(config_text).unwrap();
+        assert_eq!(cfg, Config::default());
+
+        let config_text = r#"
+---
+commands:
+    - ["ls"]
+    - ["ls", "-a"]
+directories: [ "doge", "meow" ]
+emptyFiles:
+    - "1"
+    - "2"
+    - 3
+links:
+    nvim: "~/.config/nvim"
+copies:
+    git/config: "~/.config/git/config"
+"#;
+
+        let cfg = Config::from_yaml(config_text).unwrap();
+        assert_eq!(
+            cfg,
+            Config {
+                commands: vec![vec!["ls".to_string()], vec!["ls".to_string(), "-a".to_string()]],
+                directories: vec![PathBuf::from("doge"), PathBuf::from("meow")],
+                empty_files: vec![PathBuf::from("1"), PathBuf::from("2"), PathBuf::from("3"),],
+                links: {
+                    let mut m = HashMap::default();
+                    m.insert(PathBuf::from("nvim"), PathBuf::from("~/.config/nvim"));
+                    m
+                },
+                copies: {
+                    let mut m = HashMap::default();
+                    m.insert(PathBuf::from("git/config"), PathBuf::from("~/.config/git/config"));
+                    m
+                },
+            }
+        );
     }
 }
