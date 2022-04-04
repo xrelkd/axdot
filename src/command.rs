84d26092ce6e333986c97aadbe45f594641a5399
xrelkd <46590321+xrelkd@users.noreply.github.com>
2022-04-04 15:37:52 +0800

    feat: replace `structopt` with `clap` (#10)

--- src/command.rs
+++ src/command.rs
@@ -1,108 +1,107 @@
-use std::path::PathBuf;
+use std::{io::Write, path::PathBuf};
 
-use structopt::{clap::Shell, StructOpt};
+use clap::{IntoApp, Parser, Subcommand};
+use clap_complete::Shell;
 
 use crate::{config::Config, context::Context, error::Result, manager::Manager};
 
-#[derive(Debug, StructOpt)]
-pub struct Command {
-    #[structopt(subcommand)]
-    subcommand: SubCommand,
+#[derive(Debug, Parser)]
+#[clap(about, author, version)]
+pub struct Cli {
+    #[clap(subcommand)]
+    commands: Commands,
 }
 
-impl Command {
-    pub fn new() -> Command { Command::from_args() }
-
+impl Default for Cli {
     #[inline]
-    pub fn app_name() -> String { Command::clap().get_name().to_owned() }
+    fn default() -> Cli { Self::parse() }
+}
 
+impl Cli {
     pub fn run(self) -> Result<()> {
-        let context =
-            if self.subcommand.is_standalone() { None } else { Some(Context::from_env()?) };
-        self.subcommand.run(context)
-    }
-}
+        let context = if self.commands.is_standalone() { None } else { Some(Context::from_env()?) };
 
-#[derive(Debug, StructOpt)]
-pub enum SubCommand {
-    #[structopt(about = "Shows current version")]
-    Version,
-
-    #[structopt(about = "Shows shell completions")]
-    Completions { shell: Shell },
-
-    #[structopt(about = "Generates empty configuration")]
-    Init,
-
-    #[structopt(about = "Applies from configuration file")]
-    Apply {
-        #[structopt(long = "config", help = "Configuration file path")]
-        config: Option<PathBuf>,
-
-        #[structopt(
-            long = "replace",
-            short = "r",
-            help = "Replaces files/folders if they already exist"
-        )]
-        replace: bool,
-    },
-
-    #[structopt(about = "Shows what would be applied")]
-    DryApply {
-        #[structopt(long = "config", help = "Configuration file path")]
-        config: Option<PathBuf>,
-
-        #[structopt(
-            long = "replace",
-            short = "r",
-            help = "Replaces files/folders if they already exist"
-        )]
-        replace: bool,
-    },
-}
-
-impl SubCommand {
-    #[inline]
-    pub fn is_standalone(&self) -> bool {
-        matches!(self, SubCommand::Version | SubCommand::Completions { .. })
-    }
-
-    fn create_manager(config: Option<PathBuf>) -> Result<Manager> {
-        let config_file =
-            config.unwrap_or_else(|| PathBuf::from(format!("{}.yaml", Command::app_name())));
-
-        let config = Config::load(config_file)?;
-
-        Ok(Manager::from(config))
-    }
-
-    pub fn run(self, context: Option<Context>) -> Result<()> {
-        match (self, context) {
-            (SubCommand::Version, _) => {
-                Command::clap()
-                    .write_version(&mut std::io::stdout())
-                    .expect("failed to print version");
+        match (self.commands, context) {
+            (Commands::Version, _) => {
+                let mut stdout = std::io::stdout();
+                stdout
+                    .write_all(Self::command().render_long_version().as_bytes())
+                    .expect("failed to write to stdout");
                 Ok(())
             }
-            (SubCommand::Completions { shell }, _) => {
-                let app_name = Command::app_name();
-                Command::clap().gen_completions_to(app_name, shell, &mut std::io::stdout());
+            (Commands::Completions { shell }, _) => {
+                let mut app = Self::command();
+                let bin_name = app.get_name().to_string();
+                clap_complete::generate(shell, &mut app, bin_name, &mut std::io::stdout());
                 Ok(())
             }
-            (SubCommand::Init, _) => {
+            (Commands::Init, _) => {
                 let config = Config::default();
                 println!("{}", serde_yaml::to_string(&config).expect("Config is serializable"));
                 Ok(())
             }
-            (SubCommand::Apply { replace, config }, Some(context)) => {
+            (Commands::Apply { replace, config }, Some(context)) => {
                 let manager = Self::create_manager(config)?;
                 manager.apply(false, replace, &context)
             }
-            (SubCommand::DryApply { replace, config }, Some(context)) => {
+            (Commands::DryApply { replace, config }, Some(context)) => {
                 let manager = Self::create_manager(config)?;
                 manager.apply(true, replace, &context)
             }
             (_, None) => Ok(()),
         }
     }
+
+    fn create_manager(config: Option<PathBuf>) -> Result<Manager> {
+        let app_name = Self::command().get_name().to_string();
+
+        let config_file = config.unwrap_or_else(|| PathBuf::from(format!("{}.yaml", app_name)));
+
+        let config = Config::load(config_file)?;
+
+        Ok(Manager::from(config))
+    }
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Commands {
+    #[clap(about = "Shows current version")]
+    Version,
+
+    #[clap(about = "Shows shell completions")]
+    Completions { shell: Shell },
+
+    #[clap(about = "Generates empty configuration")]
+    Init,
+
+    #[clap(about = "Applies from configuration file")]
+    Apply {
+        #[clap(long = "config", help = "Configuration file path")]
+        config: Option<PathBuf>,
+
+        #[clap(
+            long = "replace",
+            short = 'r',
+            help = "Replaces files/folders if they already exist"
+        )]
+        replace: bool,
+    },
+
+    #[clap(about = "Shows what would be applied")]
+    DryApply {
+        #[clap(long = "config", help = "Configuration file path")]
+        config: Option<PathBuf>,
+
+        #[clap(
+            long = "replace",
+            short = 'r',
+            help = "Replaces files/folders if they already exist"
+        )]
+        replace: bool,
+    },
+}
+
+impl Commands {
+    #[inline]
+    pub fn is_standalone(&self) -> bool { matches!(self, Self::Version | Self::Completions { .. }) }
 }
